9be0474915bbf3f4df24793f89b205e4cc3887d6
satyam-tyagi-rsi <satyam.tyagi@rsystems.com>
2019-08-21 11:31:36 +0530

    Update Test file

--- test/Election.js
+++ test/Election.js
@@ -1,89 +1,79 @@
-var Election = artifacts.require("./Election.sol");
+const Election = artifacts.require("./Election.sol");
 
-contract("Election", function(accounts) {
-  var electionInstance;
+contract("Election", async (accounts) => {
+  before(async () => {
+    this.election = await Election.deployed()
+  })
 
-  it("initializes with two candidates", function() {
-    return Election.deployed().then(function(instance) {
-      return instance.candidatesCount();
-    }).then(function(count) {
-      assert.equal(count, 2);
-    });
+  it("initializes with two candidates", async () => {
+      const candidateCount = await this.election.candidatesCount();
+      assert.equal(candidateCount, 2);
+  
   });
 
-  it("it initializes the candidates with the correct values", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidates(1);
-    }).then(function(candidate) {
-      assert.equal(candidate[0], 1, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
-      return electionInstance.candidates(2);
-    }).then(function(candidate) {
-      assert.equal(candidate[0], 2, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 2", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
-    });
+  it("it initializes the candidates with the correct values", async () => {
+      const candidate1 = await this.election.candidates(1);
+      assert.equal(candidate1[0], 1, "contains the correct id");
+      assert.equal(candidate1[1], "Candidate 1", "contains the correct name");
+      assert.equal(candidate1[2], 0, "contains the correct votes count");
+      
+      const candidate2 = await this.election.candidates(2);
+      assert.equal(candidate2[0], 2, "contains the correct id");
+      assert.equal(candidate2[1], "Candidate 2", "contains the correct name");
+      assert.equal(candidate2[2], 0, "contains the correct votes count");
   });
 
-  it("allows a voter to cast a vote", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      candidateId = 1;
-      return electionInstance.vote(candidateId, { from: accounts[0] });
-    }).then(function(receipt) {
+  it("allows a voter to cast a vote", async () => {
+      const candidateId = 1;
+      const receipt = await this.election.vote(candidateId, { from: accounts[0] });
       assert.equal(receipt.logs.length, 1, "an event was triggered");
       assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
       assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
-      return electionInstance.voters(accounts[0]);
-    }).then(function(voted) {
+      
+      const voted = await this.election.voters(accounts[0]);
       assert(voted, "the voter was marked as voted");
-      return electionInstance.candidates(candidateId);
-    }).then(function(candidate) {
-      var voteCount = candidate[2];
+
+      const candidate = await this.election.candidates(candidateId);
+      const voteCount = candidate[2];
       assert.equal(voteCount, 1, "increments the candidate's vote count");
-    })
   });
 
-  it("throws an exception for invalid candiates", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.vote(99, { from: accounts[1] })
-    }).then(assert.fail).catch(function(error) {
+  it("throws an exception for invalid candiate", async () => {
+    try {
+      await this.election.vote(99, { from: accounts[1] })
+    } catch(error) {
       assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
-      return electionInstance.candidates(1);
-    }).then(function(candidate1) {
-      var voteCount = candidate1[2];
+    } finally {
+      const candidate1 = await this.election.candidates(1);
+      let voteCount = candidate1[2].toNumber();
       assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
-      return electionInstance.candidates(2);
-    }).then(function(candidate2) {
-      var voteCount = candidate2[2];
+
+      const candidate2 = await this.election.candidates(2);
+      voteCount = candidate2[2];
       assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
-    });
+    }
   });
 
-  it("throws an exception for double voting", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      candidateId = 2;
-      electionInstance.vote(candidateId, { from: accounts[1] });
-      return electionInstance.candidates(candidateId);
-    }).then(function(candidate) {
-      var voteCount = candidate[2];
+  it("throws an exception for double voting", async () => {
+      const candidateId = 2;
+      await this.election.vote(candidateId, { from: accounts[1] });
+      
+      const candidate = await this.election.candidates(candidateId);
+      let voteCount = candidate[2];
       assert.equal(voteCount, 1, "accepts first vote");
       // Try to vote again
-      return electionInstance.vote(candidateId, { from: accounts[1] });
-    }).then(assert.fail).catch(function(error) {
-      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
-      return electionInstance.candidates(1);
-    }).then(function(candidate1) {
-      var voteCount = candidate1[2];
-      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
-      return electionInstance.candidates(2);
-    }).then(function(candidate2) {
-      var voteCount = candidate2[2];
-      assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
-    });
+      try {
+        await this.election.vote(candidateId, { from: accounts[1] });
+      } catch (error) {
+        assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      } finally {
+        const candidate1 = await this.election.candidates(1);
+        voteCount = candidate1[2];
+        assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+
+        const candidate2 = await this.election.candidates(2);
+        voteCount = candidate2[2];
+        assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+      }
   });
 });
